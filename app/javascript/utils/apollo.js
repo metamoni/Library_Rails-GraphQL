01313087e243b59f0dec3cf5e9f20f961ed22a40
Monica Mateiu <monica.mateiu@gmail.com>
2019-08-20 11:03:19 +0100

    add Library component and display items on page

--- app/javascript/utils/apollo.js
+++ app/javascript/utils/apollo.js
@@ -1,19 +1,22 @@
+// client
 import { ApolloClient } from 'apollo-client';
+// cache
 import { InMemoryCache } from 'apollo-cache-inmemory';
+// links
 import { HttpLink } from 'apollo-link-http';
 import { onError } from 'apollo-link-error';
 import { ApolloLink, Observable } from 'apollo-link';
-
 export const createCache = () => {
   const cache = new InMemoryCache();
   if (process.env.NODE_ENV === 'development') {
     window.secretVariableToStoreCache = cache;
   }
   return cache;
-}
+};
 
 // get CSRF token from meta tags to pass Rails forgery protection
-const getToken = () => document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+const getToken = () =>
+  document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 const token = getToken();
 const setTokenForOperation = async operation =>
   operation.setContext({
@@ -22,6 +25,7 @@ const setTokenForOperation = async operation =>
     },
   });
 
+// link with token
 const createLinkWithToken = () =>
   new ApolloLink(
     (operation, forward) =>
@@ -29,48 +33,49 @@ const createLinkWithToken = () =>
         let handle;
         Promise.resolve(operation)
           .then(setTokenForOperation)
-          .them(() => {
+          .then(() => {
             handle = forward(operation).subscribe({
               next: observer.next.bind(observer),
               error: observer.error.bind(observer),
-              complete: observer.complete.bind(observer)
+              complete: observer.complete.bind(observer),
             });
           })
           .catch(observer.error.bind(observer));
         return () => {
           if (handle) handle.unsubscribe();
         };
-      });
+      })
   );
 
-// for production: override to send errors to the external system
-const logError = error => console.error(error);
-
+// log erors
+const logError = (error) => console.error(error);
+// create error link
 const createErrorLink = () => onError(({ graphQLErrors, networkError, operation }) => {
   if (graphQLErrors) {
     logError('GraphQL - Error', {
       errors: graphQLErrors,
       operationName: operation.operationName,
-      variables: operation.variables
+      variables: operation.variables,
     });
   }
   if (networkError) {
     logError('GraphQL - NetworkError', networkError);
   }
-});
+})
 
+// http link
 const createHttpLink = () => new HttpLink({
   uri: '/graphql',
-  credentials: 'include'
-});
+  credentials: 'include',
+})
 
 export const createClient = (cache, requestLink) => {
   return new ApolloClient({
     link: ApolloLink.from([
       createErrorLink(),
       createLinkWithToken(),
-      createHttpLink()
+      createHttpLink(),
     ]),
-    cache
+    cache,
   });
 };
